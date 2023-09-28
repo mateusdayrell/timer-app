ea310294de06e8559c7aaf86da3c1664955b67a2
Fabia <fabiasiqueira13@gmail.com>
2023-09-28 08:44:01 -0300

    add nativewind

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo", ["module:metro-react-native-babel-preset"]],
-    plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
+    plugins: [
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["nativewind/babel"],
+    ],
   };
 };
